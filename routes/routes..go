b791b9fd234dded9820c9dcfa46522bfa36f72be
Sebastian Nallar <nallar@chimi.co>
2023-09-28 13:08:31 -0300

    Removed unnecesary user handler. Added healthcheck

--- routes/routes..go
+++ routes/routes..go
@@ -10,13 +10,13 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/", middleware.AuthRequired(), handlers.HomeHandler)
-		v1.POST("/create-client", middleware.AuthRequired(), handlers.CreateClientHandler)
+		v1.GET("/health-check", handlers.HealthCheckHandler)
 
+		// Auth Routes
 		v1.POST("/register", handlers.RegisterHandler)
 		v1.POST("/log-in", handlers.LoginHandler)
 
-		v1.POST("/create-user", handlers.CreateUserHandler)
-		v1.GET("/users", handlers.GetUsersHandler)
+		// Client Routes
+		v1.POST("/create-client", middleware.AuthRequired(), handlers.CreateClientHandler)
 	}
 }
